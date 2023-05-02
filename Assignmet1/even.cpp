59ed233eec986faa375209a9d721b0f120795688
Deepanshu Chauhan <131859956+DeepanshuChauhan4422@users.noreply.github.com>
2023-05-02 10:59:42 +0530

    Create even.cpp

---
+++ Assignmet1/even.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int num =82;
+    if (num>0)
+    {
+       cout<<"the number is positive";
+        
+    }
+    else if (num<0)
+    {
+       cout<<"the number is negative";
+        
+    }
+    else
+    {
+    cout<<"the number is zero";
+        
+    }
+     }
